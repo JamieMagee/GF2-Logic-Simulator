53440b9b2c7f08f9f5fb2eb9427e4ce8eca9a251
Jamie Magee <jamie.magee@gmail.com>
2013-05-25 21:49:27 +0100

    Started report, and added 2 more example circuits
    Example circuits now test all device types

--- src/names.h
+++ src/names.h
@@ -14,35 +14,36 @@ typedef int name;
 typedef string namestring;
 typedef unsigned int length;
 
-class names{
+class names
+{
 
-private:
-	vector<namestring> namelist;	
+	private:
+		vector<namestring> namelist;
 
-public:
-  name lookup (namestring str);
-    /* Returns the internal representation of the name given in character  */
-    /* form.  If the name is not already in the name table, it is          */
-    /* automatically inserted.                                             */
+	public:
+		name lookup(namestring str);
+		/* Returns the internal representation of the name given in character  */
+		/* form.  If the name is not already in the name table, it is          */
+		/* automatically inserted.                                             */
 
-  name cvtname (namestring str);
-    /* Returns the internal representation of the name given in character  */
-    /* form.  If the name is not in the name table then 'blankname' is     */
-    /* returned.                                                           */
+		name cvtname(namestring str);
+		/* Returns the internal representation of the name given in character  */
+		/* form.  If the name is not in the name table then 'blankname' is     */
+		/* returned.                                                           */
 
-  void writename (name id);
-    /* Prints out the given name on the console                            */
+		void writename(name id);
+		/* Prints out the given name on the console                            */
 
-  int namelength (name id);
-    /* Returns length ie number of characters in given name                */
+		int namelength(name id);
+		/* Returns length ie number of characters in given name                */
 
-  namestring getnamestring(name id);
-    /* Returns the namestring for the given name                           */
+		namestring getnamestring(name id);
+		/* Returns the namestring for the given name                           */
 
-  names (void);
-    /* names initialises the name table.  This procedure is called at      */
-    /* system initialisation before any of the above procedures/functions  */
-    /* are used.                                                           */
+		names(void);
+		/* names initialises the name table.  This procedure is called at      */
+		/* system initialisation before any of the above procedures/functions  */
+		/* are used.                                                           */
 };
 
 #endif /* names_h */
