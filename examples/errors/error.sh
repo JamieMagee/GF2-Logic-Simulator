cdac7893ba20aa887b6088a11f03d95a32a78483
Jamie Magee <jamie.magee@gmail.com>
2013-06-02 23:45:12 +0100

    No need to send keystrokes if it's just recording error messages

--- examples/errors/error.sh
+++ examples/errors/error.sh
@@ -4,6 +4,6 @@ for f in *.gf2
 do
 	echo "processing $f..."
 	echo "processing $f..." >> error.txt
-	echo -e "r 50\r\nq\r\n" | ../../src/logsim $f >> error.txt
+	echo ../../src/logsim $f >> error.txt
 	echo -e '\n' >> error.txt
 done
